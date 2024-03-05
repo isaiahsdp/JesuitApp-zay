1d9b2e4d16dc72d576fbef71341e44d1f84c0c6a
isaiahsdp <isaiahsdp@gmail.com>
2024-03-05 00:06:17 -0800

    reformatted, added modal, fixed current date

--- CalendarScreen.js
+++ CalendarScreen.js
@@ -1,14 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Modal, TextInput } from 'react-native';
 import DatePicker from 'react-native-modern-datepicker';
+import { getFormatedDate } from 'react-native-modern-datepicker';
 import React, { useState } from 'react';
 
 export default function CalendarScreen({ navigation }) {
   return (
-    <View style={styles.container}>
-      <BasicUsage/>
-      <StatusBar style="auto" />
-    </View>
+      <TaskCalendar/>
   );
 }
 
@@ -21,25 +19,55 @@ const styles = StyleSheet.create({
   },
 });
 
-const BasicUsage = () => {
-    const [selectedDate, setSelectedDate] = useState('');
-  
+const TaskCalendar = () => {
+    const [selectedDate, setSelectedDate] = useState('')
+    const [tasks, setTasks] = useState({})
+    const [visible, setVisible] = useState(false)
+    const [taskTitle, setTaskTitle] = useState('');
+    const [taskDescription, setTaskDescription] = useState('');
+
+    const addTask = (date) => {
+      const newTask = {
+        id: date,
+        title: taskTitle,
+        description: taskDescription,
+      }
+    };
+
+    const today = new Date()
+    const startDate = getFormatedDate(today.setDate(today.getDate() + 1), 'YYYY/MM/DD')
+
+    const showModal = () => setVisible(true)
+    const hideModal = () => setVisible(false)
+
     return (
+      <View>
         <DatePicker
           options={{
-            backgroundColor: '#090C08',
-            textHeaderColor: '#FFA25B',
-            textDefaultColor: '#F6E7C1',
+            backgroundColor: '#FFFFFF',
+            textHeaderColor: '#000000',
+            textDefaultColor: '#000000',
             selectedTextColor: '#fff',
             mainColor: '#F4722B',
-            textSecondaryColor: '#D6C7A1',
+            textSecondaryColor: '#959494',
             borderColor: 'rgba(122, 146, 165, 0.1)',
           }}
-          current="2020-07-13"
-          selected="2020-07-23"
+          current={startDate}
+          selected={startDate}
           mode="calendar"
           minuteInterval={30}
           style={{ borderRadius: 10 }}
+          onSelectedChange={(date) => setSelectedDate(date)}
         />
+        <Button title="Add Task" onPress={showModal} />
+        <Modal
+          visible={visible}
+          animationType="slide">
+            <View style= {styles.container}>
+              <Text> {selectedDate}</Text>
+            </View>
+          <Button title="Done" onPress={hideModal} />
+        </Modal>
+      </View>
       );
   };
